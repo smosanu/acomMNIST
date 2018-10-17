ed88b09d19f5557dd3ecaee252cefd09eef6ec30
Sergiu Mosanu <sm7ed@virginia.edu>
2018-10-17 14:53:37 -0400

    add kMNIST and logg failed acomMNIST

--- acomMNIST.py
+++ acomMNIST.py
@@ -20,7 +20,7 @@ if not (os.path.isfile(LABELS) and os.path.isfile(SPRITES)):
   print("Necessary data files were not found!")
   exit(1)
 
-def conv_layer(input, size_in, size_out, name="conv"):
+def conv_layer(input, size_in, size_out, name="conv_layer"):
   with tf.name_scope(name):
     w = tf.Variable(tf.truncated_normal([5, 5, size_in, size_out], stddev=0.1), name="W")
     b = tf.Variable(tf.constant(0.1, shape=[size_out]), name="B")
@@ -29,9 +29,10 @@ def conv_layer(input, size_in, size_out, name="conv"):
     tf.summary.histogram("weights", w)
     tf.summary.histogram("biases", b)
     tf.summary.histogram("activations", act)
-    return tf.nn.max_pool(act, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+    pool = tf.nn.max_pool(act, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
+    return pool
 
-def fc_layer(input, size_in, size_out, name="fc"):
+def fc_layer(input, size_in, size_out, name="fc_layer"):
   with tf.name_scope(name):
     w = tf.Variable(tf.truncated_normal([size_in, size_out], stddev=0.1), name="W")
     b = tf.Variable(tf.constant(0.1, shape=[size_out]), name="B")
@@ -41,36 +42,25 @@ def fc_layer(input, size_in, size_out, name="fc"):
     tf.summary.histogram("activations", act)
     return act
 
-def mnist_model(learning_rate, use_two_fc, use_two_conv, hparam):
+def mnist_model(learning_rate, hparam):
   tf.reset_default_graph()
   sess = tf.Session()
 
   # Setup placeholders, and reshape the data
-  x = tf.placeholder(tf.float32, shape=[None, 784], name="x")
+  x = tf.placeholder(tf.float32, shape=[None, 28*28], name="x")
   x_image = tf.reshape(x, [-1, 28, 28, 1])
   tf.summary.image('input', x_image, 3)
   y = tf.placeholder(tf.float32, shape=[None, 10], name="labels")
 
-  if use_two_conv:
-    conv1 = conv_layer(x_image, 1, 32, "conv1")
-    conv_out = conv_layer(conv1, 32, 64, "conv2")
-  else:
-    conv_out = conv_layer(x_image, 1, 16, "conv")
+  conv1 = conv_layer(x_image,  1,  32, "conv1")
+  conv2 = conv_layer(conv1,   32,  64, "conv2")
+  conv3 = conv_layer(conv2,   64, 128, "conv3")
+  conv4 = conv_layer(conv3,  128,  10, "conv4")
 
-  flattened = tf.reshape(conv_out, [-1, 7 * 7 * 64])
+  # embedding_size = 7*7*64
+  # embedding_input = flattened
 
-
-  if use_two_fc:
-    fc1 = fc_layer(flattened, 7 * 7 * 64, 1024, "fc1")
-    relu = tf.nn.relu(fc1)
-    embedding_input = relu
-    tf.summary.histogram("fc1/relu", relu)
-    embedding_size = 1024
-    logits = fc_layer(relu, 1024, 10, "fc2")
-  else:
-    embedding_input = flattened
-    embedding_size = 7*7*64
-    logits = fc_layer(flattened, 7*7*64, 10, "fc")
+  logits = conv4
 
   with tf.name_scope("xent"):
     xent = tf.reduce_mean(
@@ -88,57 +78,48 @@ def mnist_model(learning_rate, use_two_fc, use_two_conv, hparam):
 
   summ = tf.summary.merge_all()
 
-
-  embedding = tf.Variable(tf.zeros([1024, embedding_size]), name="test_embedding")
-  assignment = embedding.assign(embedding_input)
-  saver = tf.train.Saver()
+  # embedding = tf.Variable(tf.zeros([1024, embedding_size]), name="test_embedding")
+  # assignment = embedding.assign(embedding_input)
+  # saver = tf.train.Saver()
 
   sess.run(tf.global_variables_initializer())
   writer = tf.summary.FileWriter(LOGDIR + hparam)
   writer.add_graph(sess.graph)
 
-  config = tf.contrib.tensorboard.plugins.projector.ProjectorConfig()
-  embedding_config = config.embeddings.add()
-  embedding_config.tensor_name = embedding.name
-  embedding_config.sprite.image_path = SPRITES
-  embedding_config.metadata_path = LABELS
+  # config = tf.contrib.tensorboard.plugins.projector.ProjectorConfig()
+  # embedding_config = config.embeddings.add()
+  # embedding_config.tensor_name = embedding.name
+  # embedding_config.sprite.image_path = SPRITES
+  # embedding_config.metadata_path = LABELS
   # Specify the width and height of a single thumbnail.
-  embedding_config.sprite.single_image_dim.extend([28, 28])
-  tf.contrib.tensorboard.plugins.projector.visualize_embeddings(writer, config)
+  # embedding_config.sprite.single_image_dim.extend([28, 28])
+  # tf.contrib.tensorboard.plugins.projector.visualize_embeddings(writer, config)
 
   for i in range(2001):
     batch = mnist.train.next_batch(100)
     if i % 5 == 0:
       [train_accuracy, s] = sess.run([accuracy, summ], feed_dict={x: batch[0], y: batch[1]})
+      print("step %d, training accuracy %g" % (i, train_accuracy))
       writer.add_summary(s, i)
-    if i % 500 == 0:
-      sess.run(assignment, feed_dict={x: mnist.test.images[:1024], y: mnist.test.labels[:1024]})
-      saver.save(sess, os.path.join(LOGDIR, "model.ckpt"), i)
+    # if i % 50 == 0:
+    #   sess.run(assignment, feed_dict={x: mnist.test.images[:1024], y: mnist.test.labels[:1024]})
+    #   saver.save(sess, os.path.join(LOGDIR, "model.ckpt"), i)
     sess.run(train_step, feed_dict={x: batch[0], y: batch[1]})
 
-def make_hparam_string(learning_rate, use_two_fc, use_two_conv):
-  conv_param = "conv=2" if use_two_conv else "conv=1"
-  fc_param = "fc=2" if use_two_fc else "fc=1"
-  return "lr_%.0E,%s,%s" % (learning_rate, conv_param, fc_param)
+def make_hparam_string(learning_rate):
+  return "lr_%.0E" % (learning_rate)
 
 def main():
-  # You can try adding some more learning rates
-  for learning_rate in [1E-3, 1E-4]:
-
-    # Include "False" as a value to try different model architectures
-    for use_two_fc in [True]:
-      for use_two_conv in [False, True]:
-        # Construct a hyperparameter string for each one (example: "lr_1E-3,fc=2,conv=2")
-        hparam = make_hparam_string(learning_rate, use_two_fc, use_two_conv)
+  for learning_rate in [1E-2, 1E-3, 1E-4, 1E-5]:
+        # Construct a hyperparameter string for each lr
+        hparam = make_hparam_string(learning_rate)
         print('Starting run for %s' % hparam)
 
         # Actually run with the new settings
-        mnist_model(learning_rate, use_two_fc, use_two_conv, hparam)
+        mnist_model(learning_rate, hparam)
+
   print('Done training!')
   print('Run `tensorboard --logdir=%s` to see the results.' % LOGDIR)
-  print('Running on mac? If you want to get rid of the dialogue asking to give '
-        'network permissions to TensorBoard, you can provide this flag: '
-        '--host=localhost')
 
 if __name__ == '__main__':
   main()
